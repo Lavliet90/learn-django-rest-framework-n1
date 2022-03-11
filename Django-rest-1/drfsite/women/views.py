a49c6bc88d38b40ca9e9ff1cd984f6ce6638db65
lavliet90 <lavliet90@mail.ru>
2022-03-11 19:22:00 +0100

    first checks in the framework

---
+++ Django-rest-1/drfsite/women/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.forms import model_to_dict
+from rest_framework import generics
+from .models import Women
+from .serializers import WomenSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+# class WomenAPIView(generics.ListAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+
+class WomenAPIView(APIView):
+    def get(self, request):
+        w = Women.objects.all()
+        return Response({'post': WomenSerializer(w, many=True).data})
+
+    def post(self, request):
+        serializer = WomenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        post_new = Women.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            cat_id=request.data['cat_id']
+        )
+        return Response({'post': WomenSerializer(post_new).data})
